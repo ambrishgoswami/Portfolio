e198a78b3fce47f631ebe5482b6093501b656ad3
Ambrish Goswami <ambrishgoswami0@gmail.com>
2024-09-29 21:41:34 +0530

    fourth-commit

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: '/profile.jpg',
   designation: "Software Developer",
   description: "My name is AMBRISH GOSWAMI. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'ambrishgoswami@gmail.com',
+  email: 'ambrishgoswami0@gmail.com',
   phone: '+91 8052834055',
   address: 'Lucknow Uttar Pradesh India -206017 ',
   github: 'https://github.com/ambrishgoswami',
