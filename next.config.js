6a36096d353a061c9db03958991159587e240ca1
ambrishgoswami <156570582+ambrishgoswami@users.noreply.github.com>
2024-10-05 11:02:33 +0530

    Update 3

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    domains: ['res.cloudinary.com', 'media.dev.to']
+    domains: ['res.cloudinary.com', 'media.dev.to', 'instagram.flko9-1.fna.fbcdn.net']
   }
 }
 
