a26a48cce9f795fb8c8903a7f226eace366d11ac
Ambrish goswami <156570582+ambrishgoswami@users.noreply.github.com>
2024-12-30 07:44:20 +0530

    Update contactsData

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,7 +3,7 @@ export const contactsData = {
     phone: '+91 8052834055',
     address: 'Buddheswar Rajajipuram Lucknow 226017 ',
     github: 'https://github.com/ambrishgoswami',
-    facebook: 'https://www.instagram.com/code_with_peter/',
+    facebook: 'https://www.instagram.com/divine.ambrish/',
     linkedIn: 'https://www.linkedin.com/in/goswamiambrish/',
     twitter: 'https://x.com/AmbrishGos76042',
     stackOverflow: 'https://stackoverflow.com/users/23972501/ambrish-goswami',
