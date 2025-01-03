b08c681a5bba8e66be22a5fed33f08fa9e5d805d
Ambrish goswami <156570582+ambrishgoswami@users.noreply.github.com>
2025-01-03 16:18:21 +0530

    Update1

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -24,7 +24,7 @@ function AboutSection() {
         </div>
         <div className="flex justify-center order-1 lg:order-2">
           <Image
-            src={personalData.profile}
+            src='https://www.instagram.com/p/C3NJzWVvMsN/'
             width={280}
             height={280}
             alt="Ambrish Goswami"
