2b8fd5b79410c937364a21b0716b4c9ba0e8b68c
Ambrish goswami <156570582+ambrishgoswami@users.noreply.github.com>
2025-01-04 16:29:07 +0530

    Update3

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -3,19 +3,19 @@ export const educations = [
     id: 1,
     title: "Bachelor Degree",
     duration: "2022 - Present",
-    institution: "Dr Shakuntala Mishra National Rehabilitation University",
+    institution: "(DSMNRU) Institute of Engineering and Technology Lucknow ",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2019 - 2021",
-    institution: "Sri Ram Janki Inter College Ayodhya Uttar Pradesh",
+    duration: "2020 - 2021",
+    institution: "Sri Ram Janki Inter College Ayodhya Uttar Pradesh(85.83%)",
     
   },
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2019",
-    institution: "Sri Ram Janki Girls Inter College Ayodhya Uttar Pradesh",
+    duration: "2018 - 2019",
+    institution: "Sri Ram Janki Girls Inter College Ayodhya Uttar Pradesh(82%)",
   }
 ]
