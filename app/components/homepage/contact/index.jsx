0ab8cd0bd8ebeb08d14e4ad61efab787052341c6
Ambrish goswami <156570582+ambrishgoswami@users.noreply.github.com>
2025-01-04 17:11:42 +0530

    Update6

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -3,7 +3,7 @@ import { personalData } from '@/utils/data/personal-data';
 import Link from 'next/link';
 import { BiLogoLinkedin } from "react-icons/bi";
 import { CiLocationOn } from "react-icons/ci";
-import { FaFacebook, FaStackOverflow } from 'react-icons/fa';
+import { FaInstagram, FaStackOverflow } from 'react-icons/fa';
 import { FaXTwitter } from "react-icons/fa6";
 import { IoLogoGithub, IoMdCall } from "react-icons/io";
 import { MdAlternateEmail } from "react-icons/md";
@@ -75,7 +75,7 @@ function ContactSection() {
               />
             </Link>
             <Link target="_blank" href={personalData.facebook}>
-              <FaFacebook
+              <FaInstagram
                 className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
                 size={48}
               />
