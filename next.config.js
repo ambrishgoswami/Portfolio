f841682581bdf2d0443ff041f15b6433c914ca09
Ambrish goswami <156570582+ambrishgoswami@users.noreply.github.com>
2025-01-05 11:13:25 +0530

    Update 4

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-// const path = require('path')
+const path = require('path')
  
-// module.exports = {
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, 'styles')],
-//   },
-//   images: {
-//     domains: ['res.cloudinary.com', 'media.dev.to','media2.dev.to', 'instagram.flko9-1.fna.fbcdn.net']
-//   }
-// }
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    domains: ['res.cloudinary.com', 'media.dev.to','media2.dev.to', 'instagram.flko9-1.fna.fbcdn.net']
+  }
+}
 
 
 
@@ -19,34 +19,34 @@
 //   }
 // });
 
-const path = require('path')
+// const path = require('path')
  
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.dev.to',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media2.dev.to',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'instagram.flko9-1.fna.fbcdn.net',
-        pathname: '**',
-      },
-    ],
-  },
-}
+// module.exports = {
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, 'styles')],
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//         pathname: '**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'media.dev.to',
+//         pathname: '**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'media2.dev.to',
+//         pathname: '**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'instagram.flko9-1.fna.fbcdn.net',
+//         pathname: '**',
+//       },
+//     ],
+//   },
+// }
