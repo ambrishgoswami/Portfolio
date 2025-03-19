372eb08f87e18c811802be74cbb251035860866f
Ambrish goswami <156570582+ambrishgoswami@users.noreply.github.com>
2025-03-19 17:04:35 +0530

    Update personal

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is AMBRISH GOSWAMI. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'ambrishgoswami0@gmail.com',
   phone: '+91 8052834055',
-  address: 'Lucknow Uttar Pradesh India -206017 ',
+  address: 'Lucknow Uttar Pradesh India -226017 ',
   github: 'https://github.com/ambrishgoswami',
   facebook: 'https://www.instagram.com/divine.ambrish/',
   linkedIn: 'https://www.linkedin.com/in/goswamiambrish/',
